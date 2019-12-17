4594c1815f4166791b9ea75ad88545a43bb700d0
wkgreat <wkgreat@outlook.com>
2019-12-17 15:33:55 +0800

    v0.0.1, 创建模块架构，打印模块信息

---
+++ algokit/info.py
@@ -0,0 +1,13 @@
+_module_info = """
+Author: Ke Wang
+Description: Some common utilities and alogorithms for data analysis
+Enjoy.
+"""
+
+
+def info():
+    print(_module_info)
+
+
+if __name__ == '__main__':
+    info()
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "algokit",
+    version = "0.0.1",
+    keywords = ("pip", "algokit", "algorithm"),
+    description = "common utilities and algorithm",
+    long_description = "common utilities and algorithm",
+
+    url = "https://github.com/wkgreat/algokit",
+    author = "ke wang",
+    author_email = "wkgreat@outlook.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
